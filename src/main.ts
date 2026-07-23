f317403fd9ba6620677e36ecd2cf1833fed0e62b
giveandtake349-sys <giveandtake349@gmail.com>
2026-07-23 16:42:08 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
-  const port = config.get<number>('PORT', 3000);
   const frontendUrl = config.get<string>('FRONTEND_URL', '*');
 
   app.use(helmet());
